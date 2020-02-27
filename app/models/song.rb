0aef00ca2f56f64f458540ce22b1f51f60664b00
github username <github email address>
2020-02-27 11:28:07 -0800

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,29 @@
+require 'byebug' 
 class Song < ActiveRecord::Base
-  # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  def notes_attributes=(attributes)
+    attributes.each do |attribute|
+      if !attribute.empty?
+        note = Note.create(content: attribute)
+        self.notes << note
+      end
+    end
+  end
+
+  def artist_name=(name)
+    if Artist.find_by(name: name)
+      self.artist = Artist.find_by(name: name)
+    else
+      self.artist = Artist.create(name: name)
+    end
+  end
+
+  def artist_name
+    if self.artist
+      self.artist.name
+    end
+  end
 end
